a8c53794aceaa4424314848aa8c1e4e283be7711
GathangaElijah <elyg3672@gmail.com>
2025-02-06 04:42:13 +0300

    refactor: proper printing of the output

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"lem-in/utils"
+)
 
 func main() {
-	fmt.Println("the main function")
+
+	// reading the file
+	colony := utils.FileReader("test0.txt")
+
+	fmt.Println("number_of_ants ", colony.NoOfAnts)
+	rooms := colony.Rooms
+
+	fmt.Print("the_rooms\n")
+	var x, y int
+	for _, room := range rooms{
+		for _, v := range room.HouseAndCoordinates {
+			x = v[0]
+			y = v[1]
+		}
+		fmt.Println(room.Name, x, y)
+	}
+	
+	fmt.Print("the_links\n")
+	for _, room := range rooms {
+		for k, v := range room.Link{
+			fmt.Printf("%s-%s\n", k, v)
+		}
+	}
 }
