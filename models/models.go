80942375aa73616e19dc906ae4aaf2169be66d36
lakoth <lornaakothpb@gmail.com>
2025-02-11 16:54:20 +0300

    Fix:Working Dfs and Bfs

--- models/models.go
+++ models/models.go
@@ -16,77 +16,123 @@ type Colony struct {
 
 func (c *Colony) Bfs() [][]int {
 	toBeVisited := [][]int{}
+	validPaths := [][]int{}
 
 	startPtr := c.Start
 	endPtr := c.End
 	startValue := *startPtr
 	targetValue := *endPtr
 
-	startRoom := startValue.RoomNumber
-	endRoom := targetValue.RoomNumber
-	var visitedRooms []int
-	visitedRooms = append(visitedRooms, startRoom)
-
-	for len(visitedRooms) != 0 {
-		currentRoom := visitedRooms[len(visitedRooms)-1]
-		
-		if currentRoom == endRoom {
-			fmt.Println(toBeVisited)
-			
-		}
-
-		hasBeenVisited := make(map[int]bool)
-		visitedVertex := visitedRooms[len(visitedRooms)-1]
-
-		if _, exists := c.Rooms[visitedVertex]; exists {
-			hasBeenVisited[visitedVertex] = true
-			for _, connection := range c.Rooms[visitedVertex].Connection {
-				if !hasBeenVisited[connection] {
-					visitedRooms = append(visitedRooms, visitedVertex, connection)
-					fmt.Println(visitedRooms)
-					toBeVisited = append(toBeVisited, visitedRooms)
-					toBeVisited = toBeVisited[1:]
-				}
-			}
-		}
-	}
-	return toBeVisited
-}
-
-func (c *Colony) Dfs() []int {
-	visited := []int{}
-	toBeVisited := []int{}
-
-	startPtr := c.Start
-	endPtr := c.End
-	startValue := *startPtr
-	targetValue := *endPtr
-
-	startRoom := startValue.RoomNumber
+	startRoom := []int{startValue.RoomNumber}
 	endRoom := targetValue.RoomNumber
 
 	toBeVisited = append(toBeVisited, startRoom)
-	hasBeenVisited := make([]bool, len(c.Rooms))
+	currentRoom := startRoom
 
 	for len(toBeVisited) != 0 {
-		if len(visited) > 0 && visited[len(visited)-1] == endRoom {
-			break
-		}
-		visitedVertex := toBeVisited[0]
-		for _, rooms := range c.Rooms {
-			if rooms.RoomNumber == visitedVertex {
-				hasBeenVisited[visitedVertex] = true
-				for _, connection := range rooms.Connection {
-					if !hasBeenVisited[connection] {
-						visited = append(visited, connection)
-						toBeVisited = append(toBeVisited, connection)
-						toBeVisited = toBeVisited[1:]
+		currentRoom = toBeVisited[0]
+		toBeVisited = toBeVisited[1:]
 
-					}
+		if currentRoom[len(currentRoom)-1] == endRoom {
+			validPaths = append(validPaths, currentRoom)
+			continue
+
+		}
+		hasBeenVisited := make(map[int]bool)
+		for _, room := range currentRoom {
+			hasBeenVisited[room] = true
+		}
+		if _, exists := c.Rooms[currentRoom[len(currentRoom)-1]]; exists {
+			for _, connection := range c.Rooms[currentRoom[len(currentRoom)-1]].Connection {
+				matrix := []int{}
+				matrix = append(matrix, currentRoom...)
+				if !hasBeenVisited[connection] {
+					matrix = append(matrix, connection)
+					toBeVisited = append(toBeVisited, matrix)
 				}
 			}
 		}
 	}
-
-	return visited
+	return validPaths
 }
+
+func (c *Colony) Dfs() [][]int {
+	toBeVisited := [][]int{}
+	validPaths := [][]int{}
+
+	startPtr := c.Start
+	endPtr := c.End
+	startValue := *startPtr
+	targetValue := *endPtr
+
+	startRoom := []int{startValue.RoomNumber}
+	endRoom := targetValue.RoomNumber
+
+	toBeVisited = append(toBeVisited, startRoom)
+	currentRoom := startRoom
+
+	for len(toBeVisited) != 0 {
+		currentRoom = toBeVisited[len(toBeVisited)-1]
+		toBeVisited = toBeVisited[:len(toBeVisited)-1]
+
+		if currentRoom[len(currentRoom)-1] == endRoom {
+			validPaths = append(validPaths, currentRoom)
+			continue
+
+		}
+		hasBeenVisited := make(map[int]bool)
+		for _, room := range currentRoom {
+			hasBeenVisited[room] = true
+		}
+		if _, exists := c.Rooms[currentRoom[len(currentRoom)-1]]; exists {
+			for _, connection := range c.Rooms[currentRoom[len(currentRoom)-1]].Connection {
+				matrix := []int{}
+				matrix = append(matrix, currentRoom...)
+				if !hasBeenVisited[connection] {
+					matrix = append(matrix, connection)
+					toBeVisited = append(toBeVisited, matrix)
+				}
+			}
+		}
+	}
+	fmt.Println("Dfs paths")
+	return validPaths
+}
+
+// func (c *Colony) Dfs() []int {
+// 	visited := []int{}
+// 	toBeVisited := []int{}
+
+// 	startPtr := c.Start
+// 	endPtr := c.End
+// 	startValue := *startPtr
+// 	targetValue := *endPtr
+
+// 	startRoom := startValue.RoomNumber
+// 	endRoom := targetValue.RoomNumber
+
+// 	toBeVisited = append(toBeVisited, startRoom)
+// 	hasBeenVisited := make([]bool, len(c.Rooms))
+
+// 	for len(toBeVisited) != 0 {
+// 		if len(visited) > 0 && visited[len(visited)-1] == endRoom {
+// 			break
+// 		}
+// 		currentRoom := toBeVisited[0]
+// 		for _, rooms := range c.Rooms {
+// 			if rooms.RoomNumber == currentRoom {
+// 				hasBeenVisited[currentRoom] = true
+// 				for _, connection := range rooms.Connection {
+// 					if !hasBeenVisited[connection] {
+// 						visited = append(visited, connection)
+// 						toBeVisited = append(toBeVisited, connection)
+// 						toBeVisited = toBeVisited[1:]
+
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return visited
+// }
