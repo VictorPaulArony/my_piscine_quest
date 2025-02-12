5c00388ff750fa33c0068f7e595f22de5fcf8831
victorpaularony <smallypauls@gmail.com>
2025-02-13 02:30:03 +0300

    UPDATE: main.go to only have function to print

--- main.go
+++ main.go
@@ -13,48 +13,7 @@ func main() {
 		fmt.Println("Usage: go run . [FILE]")
 		return
 	}
-	file := args[1]
 
-	colony := utils.FileReader(file)
-	fmt.Println("no_ants ", colony.NoOfAnts)
-	fmt.Println("Start Room ", colony.StartRoom)
-	fmt.Println("End Room ", colony.EndRoom)
-
-	the_rooms := make(map[string][]int)
-	// var the_links [][]string
-	for _, room := range colony.Rooms {
-		for r, coord := range room.HouseAndCoordinates {
-			the_rooms[r] = coord
-		}
-	}
-
-	fmt.Println("the_rooms")
-	for r, coord := range the_rooms {
-		fmt.Printf("%v %v %v\n", r, coord[0], coord[1])
-	}
-	fmt.Println("the_links")
-	for _, room := range colony.Rooms {
-		for _, v := range room.Link {
-			fmt.Printf("%v-%v\n", v[0], v[1])
-		}
-		break
-	}
-
-	roomAndConnections := map[string][]string{}
-	for _, room := range colony.Rooms {
-		for k, v := range room.RoomAndConnectedLinks {
-			roomAndConnections[k] = v
-		}
-	}
-	// for k, v := range roomAndConnections{
-	// 	fmt.Printf("%s connected to %v\n", k, v)
-	// }
-
-	startRoom := colony.StartRoom
-	endRoom := colony.EndRoom
-
-	allPath := colony.Bfs()
-
-	bestPaths := utils.FilterPath(allPath, startRoom.Name, endRoom.Name)
-	fmt.Println(bestPaths)
+	fileName := args[1]
+	utils.Printout(fileName)
 }
