fb406018cf795081149cd08f501a80b5b5fa644a
GathangaElijah <elyg3672@gmail.com>
2025-02-12 11:04:05 +0300

    feat: defined the storage structure of the data

--- models/models.go
+++ models/models.go
@@ -1,3 +1,16 @@
 package models
 
 // space for the structs to be written
+type Colony struct {
+	NoOfAnts int
+	Rooms []*Room
+	StartRoom Room
+	EndRoom Room
+}
+
+type Room struct{
+	Name string // A room identifier
+	HouseAndCoordinates map[string][]int // A house and the coordinates
+	Link [][]string // This stores connections of room to another room
+	RoomAndLinks map[string][]string
+}
